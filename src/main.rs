1da3b35f21edc51a33505a3a91cef65670c67a67
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2025-10-15 00:07:58 -0400

    Progress!

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+use std::{env, fs, path::PathBuf};
+
 use clap::Parser;
+use markdown::{mdast::Node, Constructs, ParseOptions};
 
 #[derive(Parser, Debug)]
 #[clap(author = "TaeruAlethea", version, about)]
 /// Application configuration
 struct Args {
     /// whether to be verbose
-    #[arg(short = 'v')]
+    #[arg(short)]
     verbose: bool,
 
-    /// an optional name to greet
-    #[arg()]
-    name: Option<String>,
+    #[arg(short)]
+    input_file: Option<PathBuf>,
+
+    #[arg(short)]
+    output_dir: Option<PathBuf>,
 }
 
 fn main() {
@@ -18,8 +23,67 @@ fn main() {
     if args.verbose {
         println!("DEBUG {args:?}");
     }
-    println!(
-        "Hello {} (from coil-markdown)!",
-        args.name.unwrap_or("world".to_string())
+
+    let input_file = args.input_file.expect("No input file provided!");
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let target = current_dir.join(&input_file);
+    println!("target: {:?}", target);
+    let data = fs::read_to_string(target).expect(
+        "Unable to read file. Upward directory traversal is not allowed, so verify its not that.",
     );
+
+    let options = &ParseOptions {
+        constructs: CONSTRUCTS_FM_CF_ONLY,
+        gfm_strikethrough_single_tilde: false,
+        math_text_single_dollar: false,
+        ..ParseOptions::default()
+    };
+
+    let mdastdata = markdown::to_mdast(data.as_str(), options).unwrap();
+
+    let root = match mdastdata {
+        Node::Root(root) => root,
+        other => unimplemented!("{other:?}"),
+    };
+
+    println!("success! Root is {root:#?}");
 }
+
+const CONSTRUCTS_FM_CF_ONLY: Constructs = Constructs {
+    frontmatter: true,
+    code_fenced: true,
+    code_indented: true,
+    code_text: true,
+
+    character_reference: true,
+
+    attention: false,
+    autolink: false,
+    block_quote: false,
+    character_escape: false,
+    definition: false,
+    gfm_autolink_literal: false,
+    gfm_footnote_definition: false,
+    gfm_label_start_footnote: false,
+    gfm_strikethrough: false,
+    gfm_table: false,
+    gfm_task_list_item: false,
+    hard_break_escape: false,
+    hard_break_trailing: false,
+    heading_atx: false,
+    heading_setext: false,
+    html_flow: false,
+    html_text: false,
+    label_start_image: false,
+    label_start_link: false,
+    label_end: false,
+    list_item: false,
+    math_flow: false,
+    math_text: false,
+    mdx_esm: false,
+    mdx_expression_flow: false,
+    mdx_expression_text: false,
+    mdx_jsx_flow: false,
+    mdx_jsx_text: false,
+    thematic_break: false,
+};
---
+++ src/sample.md
@@ -0,0 +1,36 @@
+---
+author: test
+coil:
+	options:
+		- keep-indention: true
+	files:
+		- Alias1: ./path/to/file/file.cs
+		- alias2: ./path/to/file/file2.txt
+title: unaffect tag
+---
+
+Normal text that should not be parsed!
+
+Test code is not verified to be actual C# code. It shouldn't have to be.
+```c# coil.files.Alias1
+    Method(){
+      Console.writeline("test");
+    }
+```
+
+Normal text
+`An inline that should also be ignored`
+
+```js coil.files.alias2 some,other.meta
+  Hightlighting doesn't have to match the file type.
+  A didifferent set of hightlighting
+```
+
+Normal text `with inline` this should be handled nicely.
+
+```python coil: { options.keep-indention: false, files.target: "./path/to/file/file3.py" }
+  This should not be indented
+    this should not be indented
+```
+
+This should also be ignored
